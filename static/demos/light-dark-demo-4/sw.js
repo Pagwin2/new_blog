8fc543f0098e3a091e155379a36bbb94680c760d
Pagwin <pgp2024@pagwin.xyz>
2025-08-22 13:56:52 -0400

    return

--- static/demos/light-dark-demo-4/sw.js
+++ static/demos/light-dark-demo-4/sw.js
@@ -3,7 +3,7 @@ self.addEventListener("fetch", event =>{
     event.respondWith((async ()=>{
         const resp = await fetch(event.request);
         const body = await resp.text();
-        new Response(body.replace("<body>", '<body class="toggled">'), {
+        return new Response(body.replace("<body>", '<body class="toggled">'), {
             ...resp
         });
     })());
