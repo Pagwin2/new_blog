3785258c57a9411dc73f6a9cf6c8345bb6e9e668
Pagwin <pgp2024@pagwin.xyz>
2025-09-04 16:41:49 -0400

    mmmmmm

--- static/demos/light-dark-demo-4/sw.js
+++ static/demos/light-dark-demo-4/sw.js
@@ -21,7 +21,7 @@ self.addEventListener("fetch", event =>{
 async function handle_redirect(req){
     const go_back_to = req.headers.get('Referer') || '/';;
     console.log("ref:",req.headers.get('Referer'), go_back_to);
-    console.log("headers:",req.headers)
+    console.log('All headers:', [...req.headers.entries()]);
     const db_req = self.indexedDB.open("light-dark-store");
 
 
